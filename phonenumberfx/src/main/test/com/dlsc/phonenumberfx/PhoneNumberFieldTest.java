e7723be76aa4224baba6936f3884ae091344d96e
Dirk Lemmermann <dlemmermann@gmail.com>
2023-12-01 12:31:21 +0100

    Created directory for unit test.

---
+++ phonenumberfx/src/main/test/com/dlsc/phonenumberfx/PhoneNumberFieldTest.java
@@ -0,0 +1,15 @@
+package com.dlsc.phonenumberfx;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PhoneNumberFieldTest {
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void shouldDoThis() {
+    }
+}
